b6e3398f987aa32ca07660044c1aab264558b1b7
Maciej Cepielik <maciej.cepielik@gmail.com>
2023-06-12 22:49:41 +0200

    new functions added

--- js/script.js
+++ js/script.js
@@ -1,11 +1,26 @@
-const button = document.querySelector(".js-button")
-const body = document.body
-const themeName = document.querySelector(".js-button--themeName")
+{
+    const welcome = () => {
+        console.log("Hello!");
+    }
 
-button.addEventListener("click", () => {
-    body.classList.toggle("body--dark");
+    const toggleBackground = () => {
+        const body = document.body;
+        const themeName = document.querySelector(".js-button--themeName");
 
-    themeName.innerText = body.classList.contains("body--dark")
-    ? "Jasne"
-    : "Ciemne"
-})
+        body.classList.toggle("body--dark");
+
+        themeName.innerText = body.classList.contains("body--dark")
+            ? "Jasne"
+            : "Ciemne"
+    }
+
+    const init = () => {
+        const button = document.querySelector(".js-button")
+
+        button.addEventListener("click", toggleBackground);
+
+        welcome();
+    }
+
+    init();
+}
